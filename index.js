1af5e380c8ab26060f4aebb73ec07c0d8e2bdaf8
Briandvlpr <brianlagranda@gmail.com>
2024-01-25 14:18:24 -0300

    feat: /movie and /tv path for searching different types

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.json('Hello World!');
 });
 
-app.get('/data', async (req, res) => {
+app.get('/movie', async (req, res) => {
     try {
         const searchTerm = req.query.query;
 
@@ -27,6 +27,35 @@ app.get('/data', async (req, res) => {
         const options = {
             method: 'GET',
             url: `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+                searchTerm
+            )}`,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+        };
+
+        const response = await axios.request(options);
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/tv', async (req, res) => {
+    try {
+        const searchTerm = req.query.query;
+
+        if (!searchTerm) {
+            return res
+                .status(400)
+                .json({ error: 'Query parameter is required' });
+        }
+
+        const options = {
+            method: 'GET',
+            url: `https://api.themoviedb.org/3/search/tv?query=${encodeURIComponent(
                 searchTerm
             )}`,
             headers: {
